2756386fee9e8ee0c824077d5f855ccf4cebac4c
Landon Thull <landonthull.tx@gmail.com>
2023-11-28 07:28:32 -0800

    use URL datatype instead of string

--- src/crawler/helpers/PageQueue.ts
+++ src/crawler/helpers/PageQueue.ts
@@ -1,29 +1,39 @@
 export class PageQueue {
-  visited: Set<string>;
-  toVisit: Set<string>;
+	visited: Set<URL>;
+	toVisit: Set<URL>;
 
-  constructor() {
-    this.visited = new Set<string>();
-    this.toVisit = new Set<string>();
-  }
+	constructor() {
+		this.visited = new Set<URL>();
+		this.toVisit = new Set<URL>();
+	}
 
-  next(): string | null {
-    // return null if no links left
-    if (this.toVisit.size == 0) {
-      return null;
-    }
+	/**
+	 * Returns the next link in the page queue
+	 *
+	 * @returns the next link in the page queue or null
+	 */
+	next(): URL | null {
+		// return null if no links left
+		if (this.toVisit.size == 0) {
+			return null;
+		}
 
-    let nextUrl = this.toVisit.values().next().value;
+		let nextUrl = this.toVisit.values().next().value;
 
-    this.toVisit.delete(nextUrl);
-    this.visited.add(nextUrl);
+		this.toVisit.delete(nextUrl);
+		this.visited.add(nextUrl);
 
-    return nextUrl;
-  }
+		return nextUrl;
+	}
 
-  add(url: string): void {
-    if (!this.visited.has(url) && !this.toVisit.has(url)) {
-      this.toVisit.add(url);
-    }
-  }
+	/**
+	 * Adds a url to the page queue
+	 *
+	 * @param url the url to add to the page queue
+	 */
+	add(url: URL): void {
+		if (!this.visited.has(url) && !this.toVisit.has(url)) {
+			this.toVisit.add(url);
+		}
+	}
 }
